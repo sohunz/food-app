21621d852b92047f352c012f10def6bc24bfec64
sohunz <sohunily@gmail.com>
2024-05-07 08:47:59 +0700

    change some

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,7 +11,6 @@ import { FaUserFriends, FaWallet } from "react-icons/fa";
 import { useState } from "react";
 
 const Navbar = () => {
-
     const [nav, setNav] = useState(false);
     return (
         <div className="max-w-[1640px] mx-auto flex justify-between items-center p-4">
@@ -33,17 +32,16 @@ const Navbar = () => {
 
             {/* search input */}
             <div className="bg-gray-200 rounded-full px-2 flex items-center w-[200px] sm:w-[400px] lg:w-[500px]">
-                <AiOutlineSearch size={22} className="text-orange-600"/>
+                <AiOutlineSearch size={22} className="text-orange-600" />
                 <input
                     className="bg-transparent p-2 w-full focus:outline-none"
                     type="text"
                     placeholder="Search foods"
-                
                 />
             </div>
 
             {/* cart button */}
-            <button className="bg-orange-600 text-white hidden md:flex items-center py-2 gap-2 rounded-full outline-none border-none" >
+            <button className="bg-orange-600 text-white hidden md:flex items-center py-2 gap-2 rounded-full outline-none border-none">
                 <BsFillCartFill size={20} />
                 Cart
             </button>
@@ -51,19 +49,26 @@ const Navbar = () => {
             {/* moblie menu */}
             {/* overlay */}
 
-            {nav ? <div className="bg-black/80 fixed w-full h-screen z-10 top-0 left-0"></div> : ''}
-
-
-            
+            {nav ? (
+                <div className="bg-black/80 fixed w-full h-screen z-10 top-0 left-0"></div>
+            ) : (
+                ""
+            )}
 
             {/* side drawer menu */}
-            <div className={nav ? "fixed top-0 left-0 w-[300px] h-screen bg-white z-10 duration-300" : "fixed top-0 left-[-100%] w-[300px] h-screen bg-white z-10 duration-300"}>
+            <div
+                className={
+                    nav
+                        ? "fixed top-0 left-0 w-[300px] h-screen bg-white z-10 duration-300"
+                        : "fixed top-0 left-[-100%] w-[300px] h-screen bg-white z-10 duration-300"
+                }
+            >
                 <div>
-                <AiOutlineClose
-                    size={25}
-                    className="absolute right-4 top-4 cursor-pointer"
-                    onClick={() => setNav(!nav)}
-                />
+                    <AiOutlineClose
+                        size={25}
+                        className="absolute right-4 top-4 cursor-pointer"
+                        onClick={() => setNav(!nav)}
+                    />
                 </div>
                 <h2 className="text-2xl p-4">
                     Best <span className="font-bold text-orange-600">Eats</span>
@@ -71,28 +76,53 @@ const Navbar = () => {
                 <nav>
                     <ul className="flex flex-col p-4 text-gray-800">
                         <li className="text-xl py-4 flex">
-                            <TbTruckDelivery size={25} className="mr-4 text-orange-600" />{" "}
+                            <TbTruckDelivery
+                                size={25}
+                                className="mr-4 text-orange-600"
+                            />{" "}
                             Orders
                         </li>
                         <li className="text-xl py-4 flex">
-                            <MdFavorite size={25} className="mr-4 text-orange-600" /> Favorites
+                            <MdFavorite
+                                size={25}
+                                className="mr-4 text-orange-600"
+                            />{" "}
+                            Favorites
                         </li>
                         <li className="text-xl py-4 flex">
-                            <FaWallet size={25} className="mr-4 text-orange-600" /> Wallet
+                            <FaWallet
+                                size={25}
+                                className="mr-4 text-orange-600"
+                            />{" "}
+                            Wallet
                         </li>
                         <li className="text-xl py-4 flex">
-                            <MdHelp size={25} className="mr-4 text-orange-600" /> Help
+                            <MdHelp
+                                size={25}
+                                className="mr-4 text-orange-600"
+                            />{" "}
+                            Help
                         </li>
                         <li className="text-xl py-4 flex">
-                            <AiFillTag size={25} className="mr-4 text-orange-600" /> Promotions
+                            <AiFillTag
+                                size={25}
+                                className="mr-4 text-orange-600"
+                            />{" "}
+                            Promotions
                         </li>
                         <li className="text-xl py-4 flex">
-                            <BsFillSaveFill size={25} className="mr-4 text-orange-600" /> Best
-                            One
+                            <BsFillSaveFill
+                                size={25}
+                                className="mr-4 text-orange-600"
+                            />{" "}
+                            Best One
                         </li>
                         <li className="text-xl py-4 flex">
-                            <FaUserFriends size={25} className="mr-4 text-orange-600" /> Invite
-                            Friends
+                            <FaUserFriends
+                                size={25}
+                                className="mr-4 text-orange-600"
+                            />{" "}
+                            Invite Friends
                         </li>
                     </ul>
                 </nav>
